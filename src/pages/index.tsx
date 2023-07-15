c71dedd2f7e49d9c33d50c01f417090c4369ed4e
Akhil Waghmare <akhilwaghmare@gmail.com>
2023-07-15 11:39:46 -0700

    Starter code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,59 +1,7 @@
-import VideoPlayerControls from "@/components/VideoPlayerControls";
-import { useEffect, useRef, useState } from "react";
-
 export default function Home() {
-  const [videoProgress, setVideoProgress] = useState<number>(0);
-  const [videoDuration, setVideoDuration] = useState<number>();
-  const [isPaused, setIsPaused] = useState(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    const video = videoRef.current;
-    if (video) {
-      setVideoDuration(video.duration);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (isPaused) return;
-    const currentTime = videoRef.current?.currentTime;
-    if (videoDuration != null && currentTime != null) {
-      let loadingTimeout = setTimeout(() => {
-        if (videoProgress == currentTime / videoDuration) {
-          setVideoProgress((prev) => prev + 0.000001);
-        } else {
-          setVideoProgress(currentTime / videoDuration);
-        }
-      }, 10);
-
-      return () => {
-        clearTimeout(loadingTimeout);
-      };
-    }
-  }, [videoProgress, videoDuration, isPaused]);
-
-  const togglePlayPause = () => {
-    const video = videoRef.current;
-    if (video) {
-      setIsPaused(!video.paused);
-      video.paused ? video.play() : video.pause();
-    }
-  };
-
   return (
     <main>
-      <div className="relative w-[90%] mx-auto my-8 rounded-xl overflow-hidden">
-        <div className="absolute top-4 right-4 z-10">
-          <VideoPlayerControls
-            progress={videoProgress}
-            isPaused={isPaused}
-            onPlayPause={togglePlayPause}
-          />
-        </div>
-        <video className="w-full" ref={videoRef} loop muted autoPlay>
-          <source src="/video.mp4" />
-        </video>
-      </div>
+      <h1>Custom video player project</h1>
     </main>
   );
 }
